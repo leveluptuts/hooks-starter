9e1eaf2fae57df756f5bf9b5f8279a472a1944b8
Scott Tolinski <scott.tolinski@gmail.com>
2019-02-18 14:23:43 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="main-wrapper">
+      <h1>Level Up Dishes</h1>
+    </div>
+  );
+};
 
 export default App;

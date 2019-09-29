8c5459a463a29bb04a4877c982b69d410ae4f7fc
Andy Muniz <munizandy94@gmail.com>
2019-09-29 17:04:16 -0500

    useState Hook example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+
+  const [name, setName] = useState('');
+
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
+      <h3>{name}</h3>
+      <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
     </div>
   );
 };

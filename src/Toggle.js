18d2aed514306e5e1f0d6f6d93c04ac628b7efb4
Andy Muniz <munizandy94@gmail.com>
2019-09-29 17:15:58 -0500

    Refactored class into a function component w/State

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,22 +1,13 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-export default class Refactor extends Component {
-  state = {
-    isToggled: false
-  };
-
-  toggle = () => {
-    this.setState(state => {
-      return { isToggled: !state.isToggled };
-    });
-  };
-
-  render() {
+const Toggle = () => {
+    const [isToggled, setToggle] = useState(false);
     return (
-      <div>
-        <button onClick={this.toggle}>Toggle</button>
-        {this.state.isToggled && <h2>Hello!</h2>}
-      </div>
+        <div>
+            <button onClick={(e) => setToggle(!isToggled)}>Toggle</button>
+            {isToggled && <h2>Hello</h2>}
+        </div>
     );
-  }
-}
+};
+
+export default Toggle;

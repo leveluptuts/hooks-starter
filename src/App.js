48f4c646c877e8ad2eefbf0af8df4cc5978c33b4
Andy Muniz <munizandy94@gmail.com>
2019-09-29 17:52:50 -0500

    useRef example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Toggle from './Toggle';
 import { useTitleInput } from './hooks/useTitleInput';
 
 const App = () => {
   const [name, setName] = useTitleInput('');
+  const ref = useRef();
+
   return (
-    <div className="main-wrapper">
-      <h1>Level Up Dishes</h1>
+    <div className="main-wrapper" ref={ref}>
+      <h1 onClick={() => ref.current.classList.add('new-fake-class')}>Level Up Dishes</h1>
       <Toggle></Toggle>
       <form onSubmit={(e) => {
         e.preventDefault();

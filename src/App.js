2ece31b8f021c9340371b43254cff2fa9b9e91cc
Andy Muniz <munizandy94@gmail.com>
2019-09-29 17:33:15 -0500

    useState and useEffect examples

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Toggle from './Toggle';
 
 const App = () => {
-
   const [name, setName] = useState('');
+
+  useEffect(() => {
+    document.title = name;
+  });
 
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
       <Toggle></Toggle>
-      <h3>{name}</h3>
-      <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        formSubmit(name, setName);
+      }}>
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
+
+const formSubmit = (value, setValue) => {
+  console.log('email sent to ' + value + '!');
+  setValue('');
+}
 
 export default App;

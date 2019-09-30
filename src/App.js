3b85eff3b190160023da7662b3bf8d5af96ec365
Andy Muniz <munizandy94@gmail.com>
2019-09-29 22:22:46 -0500

    useContext example

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, createContext } from 'react';
 import Toggle from './Toggle';
 import { useTitleInput } from './hooks/useTitleInput';
+
+export const UserContext = createContext();
 
 const App = () => {
   const [name, setName] = useTitleInput('');
   const ref = useRef();
 
   return (
-    <div className="main-wrapper" ref={ref}>
-      <h1 onClick={() => ref.current.classList.add('new-fake-class')}>Level Up Dishes</h1>
-      <Toggle></Toggle>
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        formSubmit(name, setName);
-      }}>
-        <input
-          type="text"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-        />
-        <button>Submit</button>
-      </form>
-    </div>
+    <UserContext.Provider className="Provider"
+      value={{
+        user: false
+      }}
+    >
+      <div className="main-wrapper" ref={ref}>
+        <h1 onClick={() => ref.current.classList.add('new-fake-class')}>Level Up Dishes</h1>
+        <Toggle></Toggle>
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          formSubmit(name, setName);
+        }}>
+          <input
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <button>Submit</button>
+        </form>
+      </div>
+    </UserContext.Provider>
   );
 };
 
--- src/Toggle.js
+++ src/Toggle.js
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { UserContext } from './App';
 
 const Toggle = () => {
     const [isToggled, setToggle] = useState(false);
+    const userInfo = useContext(UserContext);
+    console.log('userInfo: ', userInfo);
+
+    if(!userInfo.user) return null;
     return (
         <div>
             <button onClick={(e) => setToggle(!isToggled)}>Toggle</button>

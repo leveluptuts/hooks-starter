471a16fd58d88eec982cb3cd746bc4bbc8211fb3
Andy Muniz <munizandy94@gmail.com>
2019-10-02 21:31:06 -0500

    useDebugValue example

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useMemo } from 'react';
+import React, { useState, useEffect, useRef, useMemo, useDebugValue } from 'react';
 import Toggle from './Toggle';
 import { useTitleInput } from './hooks/useTitleInput';
 
@@ -6,17 +6,9 @@ const App = () => {
   const [name, setName] = useTitleInput('');
   const ref = useRef();
 
-  const reverseWord = word => {
-    console.log('function called');
-    return word.split("").reverse().join("");
-  }
-
-  const title = 'Level Up Dishes';
-  const TitleReversed = useMemo(()=> reverseWord(title), [title]);
-
   return (
     <div className="main-wrapper" ref={ref}>
-      <h1 onClick={() => ref.current.classList.add('new-fake-class')}>{TitleReversed}</h1>
+      <h1 onClick={() => ref.current.classList.add('new-fake-class')}>Level Up Dishes</h1>
       <Toggle></Toggle>
       <form onSubmit={(e) => {
         e.preventDefault();
--- src/hooks/useTitleInput.js
+++ src/hooks/useTitleInput.js
@@ -1,10 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useDebugValue } from 'react';
 
 function useTitleInput(initialValue) {
     const [value, setValue] = useState(initialValue);
     useEffect(() => {
         document.title = value;
     });
+
+    useDebugValue(value.length > 0 ? 'Full' : 'Empty');
+
     return [value, setValue];
 }
 

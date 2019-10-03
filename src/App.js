ff98dfa4f836d8ce3034624f78c411dfea6b3612
Andy Muniz <munizandy94@gmail.com>
2019-10-02 21:50:48 -0500

    useEffect with fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React, { useState, useEffect, useRef, useMemo, useDebugValue } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Toggle from './Toggle';
 import { useTitleInput } from './hooks/useTitleInput';
 
 const App = () => {
   const [name, setName] = useTitleInput('');
   const ref = useRef();
+
+  const [dishes, setDishes] = useState([]);
+
+  const fetchDishes = async () => {
+    console.log('fetching dishes');
+    const res = await fetch('https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes');
+    const data = await res.json();
+    setDishes(data);
+  };
+
+  useEffect(() => {
+    fetchDishes();
+  }, []);
 
   return (
     <div className="main-wrapper" ref={ref}>
@@ -21,6 +34,19 @@ const App = () => {
         />
         <button>Submit</button>
       </form>
+
+      {dishes.map(dish => (
+        <article className="dish-card dish-card--withImage">
+          <h3>{dish.name}</h3>
+          <p>{dish.desc}</p>
+          <div className="ingredients">
+            {dish.ingredients.map(ingredient => (
+              <span>{ingredient}</span>
+            ))}
+          </div>
+        </article>
+      ))}
+
     </div>
   );
 };
